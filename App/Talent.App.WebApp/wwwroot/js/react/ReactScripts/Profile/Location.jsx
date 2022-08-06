7939e59e1300971e2bd102c14c2dfa4587e5e2e0
Tarun301 <tksaini74@gmail.com>
2022-08-06 16:26:06 +1200

    Created the social media component functionality & dropdown

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Location.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Location.jsx
@@ -1,39 +1,161 @@
 ﻿import React from 'react'
 import Cookies from 'js-cookie'
-import { default as Countries } from '../../../../util/jsonFiles/countries.json';
+import  {Countries} from '../../../../util/jsonFiles/countries.json';
+import { Location } from '../Employer/CreateJob/Location.jsx';
 import { ChildSingleInput } from '../Form/SingleInput.jsx';
+import { Dropdown } from '../Profile/Dropdown.jsx';
 
 export class Address extends React.Component {
     constructor(props) {
         super(props)
+
+        const Address = props.Address ?
+            Object.assign({}, props.Address)
+            : {
+                number: "",
+                street: "",
+                suburb: "",
+                postcode: ""
+
+            }
+
+        this.state = {
+            showEditSection: false,
+            newMailAddress: Address
+        }
+
+        this.openEdit = this.openEdit.bind(this)
+        this.closeEdit = this.closeEdit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.saveMailAddress = this.saveMailAddress.bind(this)
+        this.renderEdit = this.renderEdit.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
         
 
+    }
+
+    openEdit() {
+        const Address = Object.assign({}, this.props.Address)
+        this.setState({
+            showEditSection: true,
+            newMailAddress: Address
+        })
+    }
+
+    closeEdit() {
+        this.setState({
+            showEditSection: false
+        })
+    }
+
+    handleChange(event) {
+        const data = Object.assign({}, this.state.newMailAddress)
+        data[event.target.name] = event.target.value
+        this.setState({
+            newMailAddress: data
+        })
+    }
+
+    saveMailAddress() {
+        console.log(this.props.componentId)
+        console.log(this.state.newMailAddress)
+        const data = Object.assign({}, this.state.newMailAddress)
+        this.props.controlFunc(this.props.componentId, data)
+        this.closeEdit()
     }
 
    
     render() {
         return (
-            <div>
+            this.state.showEditSection ? this.renderEdit() : this.renderDisplay()
+        ) 
+    }
+
+    renderEdit() {
+        return (
+            <div className='ui sixteen wide column'>
+                <ChildSingleInput
+                    inputType="text"
+                    label="Number"
+                    name="number"
+                    value={this.state.newMailAddress.number}
+                    controlFunc={this.handleChange}
+                    maxLength={40}
+                    placeholder="Enter your House Number"
+                    errorMessage="Please enter a valid House Number"
+                />
+                <ChildSingleInput
+                    inputType="text"
+                    label="Street"
+                    name="street"
+                    value={this.state.newMailAddress.street}
+                    controlFunc={this.handleChange}
+                    maxLength={80}
+                    placeholder="Enter your street name"
+                    errorMessage="Please enter a valid street name"
+                />
+                <ChildSingleInput
+                    inputType="text"
+                    label="Suburb"
+                    name="suburb"
+                    value={this.state.newMailAddress.suburb}
+                    controlFunc={this.handleChange}
+                    maxLength={80}
+                    placeholder="Enter your suburb name"
+                    errorMessage="Please enter a valid suburb name"
+                />
+               
                 
+                <ChildSingleInput
+                    inputType="text"
+                    label="Post code"
+                    name="postcode"
+                    value={this.state.newMailAddress.postcode}
+                    controlFunc={this.handleChange}
+                    maxLength={80}
+                    placeholder="Enter your post code"
+                    errorMessage="Please enter a valid post code"
+                />
+                 
+                
+                <button type="button" className="ui teal button" onClick={this.saveMailAddress}>Save</button>
+                <button type="button" className="ui button" onClick={this.closeEdit}>Cancel</button>
+
             </div>
         )
-       
+    }
+
+    renderDisplay() {
+        let address = this.props.Address ? `${this.props.Address.number} ${this.props.Address.street} ${this.props.Address.suburb} ${this.props.Address.postcode}` : ""
+        
+
+
+        return (
+            <div className='row'>
+                <div className="ui sixteen wide column">
+                    <Dropdown />
+                    <button type="button" className="ui right floated teal button" onClick={this.openEdit}>Edit</button>
+                </div>
+            </div>
+        )
     }
 
 }
 
 export class Nationality extends React.Component {
-    constructor(props) {
+    constructor(props){
         super(props)
-       
-    }
-
-    
-    render() {
-        return (
-            <div></div>
-        )
 
         
+
+        
+    }
+    
+    render (){
+        return (
+            <div>
+                
+            </div>
+        )
     }
 }
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SocialMediaLinkedAccount.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SocialMediaLinkedAccount.jsx
@@ -1,26 +1,141 @@
 ﻿/* Social media JSX */
 import React from 'react';
-import { Popup } from 'semantic-ui-react';
+import { Button, Popup, Icon } from 'semantic-ui-react';
+import { ChildSingleInput } from '../Form/SingleInput.jsx';
 
 export default class SocialMediaLinkedAccount extends React.Component {
     constructor(props) {
         super(props);
 
+        const SocialMediaLinkedAccount = props.SocialMediaLinkedAccount ?
+            Object.assign({}, props.SocialMediaLinkedAccount)
+            : {
+                LinkedIn: "",
+                GitHub: ""
+            } 
+
+        this.state = {
+            showEditSection: false,
+            newLinkedAccounts: SocialMediaLinkedAccount
+        }
+
+        this.openEdit = this.openEdit.bind(this)
+        this.closeEdit = this.closeEdit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.saveLinkedAccounts = this.saveLinkedAccounts.bind(this)
+        this.renderEdit = this.renderEdit.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+
 
     }
+
+    openEdit() {
+        const SocialMediaLinkedAccount = Object.assign({}, this.props.SocialMediaLinkedAccount)
+        this.setState({
+            showEditSection: true,
+            newLinkedAccounts: SocialMediaLinkedAccount
+        })
+    }
+
+    closeEdit() {
+        this.setState({
+            showEditSection: false
+        })
+    }
+
+    handleChange(event) {
+        const data = Object.assign({}, this.state.newLinkedAccounts)
+        data[event.target.name] = event.target.value
+        this.setState({
+            newLinkedAccounts: data
+        })
+    }
+
+   
+
+        
 
     componentDidMount() {
         $('.ui.button.social-media')
-            .popup();
+            .popup(Popup);
     }
 
-   render(){
+    saveLinkedAccounts() {
+        console.log(this.props.componentId)
+        console.log(this.state.newLinkedAccounts)
+        this.closeEdit()
+    }
+
+    render() {
+        return (
+            this.state.showEditSection ? this.renderEdit() : this.renderDisplay()
+        )
+    }
+
+   renderEdit(){
     return (
-        <div>
-           
+        <div className="ui sixteen wide column">
+
+            <ChildSingleInput
+                inputType="text"
+                label="Linkedinurl"
+                name="linkedinurl"
+                value={this.state.newLinkedAccounts.LinkedIn}
+                controlFunc={this.handleChange}
+                maxLength={80}
+                placeholder="Enter your LinkedIn Url"
+                errorMessage="Please enter a valid LinkedIn Url"
+            />
+
+            <ChildSingleInput
+                inputType="text"
+                label="GitHuburl"
+                name="gitHuburl"
+                value={this.state.newLinkedAccounts.GitHub}
+                controlFunc={this.handleChange}
+                maxLength={80}
+                placeholder="Enter your GitHub Url"
+                errorMessage="Please enter a valid GitHub Url"
+            />
+
+            <button type="button" className="ui teal button" onClick={this.saveLinkedAccounts}>Save</button>
+            <button type="button" className="ui button" onClick={this.closeEdit}>Cancel</button>
+        
+            
         </div>
+
+        
     )
-   }
+
     
+   }
+   renderDisplay(){
+    let GitHub = this.props.SocialMediaLinkedAccount ? `${this.props.SocialMediaLinkedAccount.GitHub}` : ""
+    let LinkedIn = this.props.SocialMediaLinkedAccount ? `${this.props.SocialMediaLinkedAccount.LinkedIn}` : ""
+    return (
+        <div className='row'>
+            <div className="ui sixteen wide column">
+
+                <React.Fragment>
+
+                    
+                    <Button color='linkedin'>
+                    <Icon name='linkedin' /> LinkedIn
+                    <p>{LinkedIn}</p>
+                    </Button>
+
+                    <Button color='black'>
+                    <Icon name='github' /> GitHub
+                    <p>{GitHub}</p>
+                    </Button>
+
+                </React.Fragment>
+                <button type="button" className="ui right floated teal button" onClick={this.openEdit}>Edit</button>
+
+            </div>
+        </div>
+        
+    )
+}
 
 }
